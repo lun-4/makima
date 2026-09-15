e1575c5ec4646020143c47319e9efa6d359fa201
luna <git@l4.pm>
2026-09-14 21:11:11 -0300

    simplify session query check

--- plugins/sessions/init.lua
+++ plugins/sessions/init.lua
@@ -450,7 +450,7 @@ local function open(query)
     frame = 0,
     loading = true,
   }
-  if query and query ~= "" then
+  if query ~= "" then
     board.input:insert_text(query)
   end
   -- Two-phase load: live sessions are cheap, so they show up and take keys
