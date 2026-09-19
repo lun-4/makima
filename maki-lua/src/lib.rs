tontinton/maki

fix(config): let a disabled plugin hand its tool name over
Turning a bundled plugin off with `plugins.skill = { enabled = false }` already stopped it from loading, but `into_config` also copied every disabled name into `agent.disabled_tools`, a plain name filter applied to the model's tool array on every request.

So a plugin of your own could register `skill` and show up in `maki prompt --tools`, while the live agent never saw it.

Off already means never registered, so that list of names is gone and `agent.disabled_tools` now carries only what `--disallowed-tools` puts there.

Leaving the builtin on still fails the load with `NameConflict`, since the tool is really there.

Permission rules are keyed by tool name, so maki now warns at load when a plugin takes a name like `bash` or `write` and inherits the rules behind it, together with any "always allow" you saved.

Closes #779.

maki-lua/src/lib.rs
@@ -19,7 +19,7 @@ pub use api::util::command::{
 };
 pub use docs::{DocKind, FnDoc, ModuleDoc, ParamDoc, api_docs};
 pub use error::PluginError;
-pub use loader::{EventHandle, PluginHost, SKIPPED_PLUGIN_WARNING};
+pub use loader::{EventHandle, PERMISSION_NAME_WARNING, PluginHost, SKIPPED_PLUGIN_WARNING};
 pub use pack::{
     DiscoveredPackage, Discovery, InstallReport, Interaction, MANAGED_GROUP, Origin, discover,
     discover_installed, install_declared, lockfile_path, sanitize_message, site_dir,

lun-4/makima

maki-lua/src/lib.rs
@@ -24,7 +24,7 @@ pub use api::util::command::{
 pub use api::util::picker::{PickerConfig, PickerEvent, PickerItemSpec, PickerResult};
 pub use docs::{DocKind, FnDoc, ModuleDoc, ParamDoc, api_docs};
 pub use error::{PluginError, SessionOptionMutationError};
-pub use loader::{EventHandle, PluginHost, TestCompletionBackend};
+pub use loader::{EventHandle, PERMISSION_NAME_WARNING, PluginHost, TestCompletionBackend};
 pub use plugin_permissions::{Permission, PluginPermissions};
 pub use runtime::{CommandArgumentContext, CommandArgumentLifecycle};
 pub use runtime::{KILL_GRACE, RestoreItem, WARM_TOOL_CAP};