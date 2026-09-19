tontinton/maki

fix(storage): keep logging when another process rotates the log
Every maki process kept its own handle on `maki.log` plus its own byte counter, and it only looked at that handle again once its own counter passed `max_log_bytes`, which for a quiet session meant never, so a busy peer could rename the handle down to `maki.9.log` and then delete it while the quiet one wrote on into an inode with no name, and the user saw logs that stopped one day and never came back until a restart.

The counter is gone and every write asks the open file instead, one `fstat` that reports the real size, which now counts what all the processes wrote rather than only ours, and whether the file still has a name, so a handle that was rotated away or deleted gets reopened on the next line, and the logs dir is recreated on the way for the people who clear their logs by deleting the whole folder.

Four smaller traps went with it: `max_files` is clamped because zero underflowed into four billion `exists` calls made under the rotation lock that every other process waits on, the writer no longer calls `tracing::warn` because it runs with the subscriber's lock held and would deadlock on itself, lowering `max_log_files` now prunes the files above the new ceiling instead of stranding them forever, and a rotation that cannot rename waits for another `max_bytes` before trying again rather than scanning the dir once per log line, which is what Windows does every time since it refuses to rename a file you hold open.

`init_logging` also stopped swallowing its error, so an unwritable log dir says so instead of looking like an idle one, and the filter reads `MAKI_LOG` before `RUST_LOG` because `EnvFilter` drops any target no directive names, which let a `RUST_LOG=other_crate=debug` left in a shell profile for an unrelated project silence maki completely.

maki-lua/src/docs_render.rs
@@ -151,9 +151,11 @@ settings in a local table, or export a `setup(opts)` function `init.lua` calls.
 runs, an edited plugin is still the old one.
 
 To debug, add `maki.log.info|warn|error(...)` calls. They write to `maki.log`
-in the dir `maki.env.logs_dir()` returns (Linux: `~/.local/logs/maki/`). When
-a backtrace comes out useless, start maki with `--no-jit`: plugins then run on
-the interpreter, with full debug info.
+in the dir `maki.env.logs_dir()` returns (Linux: `~/.local/logs/maki/`). The
+log keeps `info` and above. Set `MAKI_LOG=debug` to also keep `maki.log.debug`,
+or `MAKI_LOG=maki_lua=trace` to narrow it to one target. When a backtrace comes
+out useless, start maki with `--no-jit`: plugins then run on the interpreter,
+with full debug info.
 
 {AGENT_NOTES}## Conventions
 

src/setup.rs
@@ -1,5 +1,6 @@
 use std::fmt::Display;
-use std::sync::Mutex;
+use std::io::{self, Write};
+use std::sync::{Mutex, MutexGuard, PoisonError};
 
 use color_eyre::Result;
 use color_eyre::eyre::{Context, eyre};
@@ -10,6 +11,12 @@ use maki_storage::StateDir;
 use maki_storage::log::RotatingFileWriter;
 use maki_storage::model::read_model;
 use tracing_subscriber::EnvFilter;
+use tracing_subscriber::filter::LevelFilter;
+use tracing_subscriber::fmt::MakeWriter;
+
+const LOG_ENV: &str = "MAKI_LOG";
+const LOG_ENV_SHARED: &str = "RUST_LOG";
+const DEFAULT_LOG_LEVEL: LevelFilter = LevelFilter::INFO;
 
 const PROVIDER_PRIORITY: &[&str] = &[
     "anthropic",
@@ -153,17 +160,129 @@ pub fn report_session_start(start_type: &'static str, session_id: Option<impl Di
     maki_otel::emit::session_started(start_type, id.as_deref());
 }
 
+/// The writer every event goes through.
+///
+/// A bare `Mutex` would do, except `tracing_subscriber`'s blanket impl unwraps
+/// the poison, so a single panic raised while a line was being written turned
+/// every later log call into a panic of its own. Taking the guard back out of
+/// the poison keeps a wounded process writing its own post mortem.
+struct SharedWriter(Mutex<RotatingFileWriter>);
+
+struct SharedGuard<'a>(MutexGuard<'a, RotatingFileWriter>);
+
+impl Write for SharedGuard<'_> {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        self.0.write(buf)
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        self.0.flush()
+    }
+}
+
+impl<'a> MakeWriter<'a> for SharedWriter {
+    type Writer = SharedGuard<'a>;
+
+    fn make_writer(&'a self) -> Self::Writer {
+        SharedGuard(self.0.lock().unwrap_or_else(PoisonError::into_inner))
+    }
+}
+
+/// `MAKI_LOG` is ours and is taken literally, `MAKI_LOG=off` included.
+///
+/// `RUST_LOG` belongs to every Rust program on the machine, and `EnvFilter`
+/// drops any target no directive names, so a `RUST_LOG=other_crate=debug` left
+/// in a shell profile for an unrelated project silenced maki entirely. We still
+/// read it, and a value that names a level decides ours, but a value that only
+/// lists other people's targets gets our level added underneath, so a foreign
+/// variable can add to our logs and never take them away.
+fn log_filter() -> EnvFilter {
+    let read = |name: &str| std::env::var(name).ok().filter(|v| !v.trim().is_empty());
+    build_log_filter(read(LOG_ENV).as_deref(), read(LOG_ENV_SHARED).as_deref())
+}
+
+/// Pure core of `log_filter`, so tests never have to set a process wide
+/// variable and race every other test in the binary.
+fn build_log_filter(ours: Option<&str>, shared: Option<&str>) -> EnvFilter {
+    for (name, value, is_ours) in [(LOG_ENV, ours, true), (LOG_ENV_SHARED, shared, false)] {
+        let Some(value) = value else {
+            continue;
+        };
+        match EnvFilter::builder().parse(value) {
+            Ok(filter) if is_ours || sets_a_level(value) => return filter,
+            Ok(filter) => return filter.add_directive(DEFAULT_LOG_LEVEL.into()),
+            Err(error) => eprintln!("maki: ignoring {name}={value:?}: {error}"),
+        }
+    }
+    EnvFilter::default().add_directive(DEFAULT_LOG_LEVEL.into())
+}
+
+/// Whether a filter string carries a bare level such as `debug`, as opposed to
+/// only per target directives like `foo=debug`.
+///
+/// Empty pieces are skipped here, as `EnvFilter` skips them too. `LevelFilter`
+/// reads an empty string as `error`, so the trailing comma in
+/// `RUST_LOG=other_crate=debug,` would look like a level and cost us the log.
+fn sets_a_level(value: &str) -> bool {
+    value
+        .split(',')
+        .map(str::trim)
+        .any(|part| !part.is_empty() && part.parse::<LevelFilter>().is_ok())
+}
+
+/// Logging is the first thing to set up and the last thing allowed to stop the
+/// program, but a failure here used to be swallowed whole, so an unwritable log
+/// dir looked exactly like an idle one: no logs, no reason, no clue.
 pub fn init_logging(storage_config: &maki_config::StorageConfig) {
-    let Ok(writer) =
-        RotatingFileWriter::new(storage_config.max_log_bytes, storage_config.max_log_files)
-    else {
-        return;
-    };
-    let writer = Mutex::new(writer);
-    let filter = EnvFilter::try_from_env("RUST_LOG").unwrap_or_else(|_| EnvFilter::new("info"));
+    let writer =
+        match RotatingFileWriter::new(storage_config.max_log_bytes, storage_config.max_log_files) {
+            Ok(writer) => writer,
+            Err(error) => {
+                eprintln!("maki: logging disabled, cannot open the log file: {error}");
+                return;
+            }
+        };
     tracing_subscriber::fmt()
         .json()
-        .with_env_filter(filter)
-        .with_writer(writer)
+        .with_env_filter(log_filter())
+        .with_writer(SharedWriter(Mutex::new(writer)))
         .init();
+}
+
+#[cfg(test)]
+mod tests {
+    use test_case::test_case;
+
+    use super::{DEFAULT_LOG_LEVEL, build_log_filter};
+
+    const FOREIGN: &str = "other_crate=debug";
+    const FOREIGN_EMPTY_PIECES: &str = ",other_crate=debug,,";
+    const BROKEN: &str = "not a directive=!!";
+
+    #[test_case(None, None, "info" ; "nothing_set_logs_at_info")]
+    #[test_case(Some("warn"), None, "warn" ; "our_variable_wins")]
+    #[test_case(Some("off"), None, "off" ; "our_variable_can_silence_us")]
+    #[test_case(Some("warn"), Some("trace"), "warn" ; "our_variable_beats_the_shared_one")]
+    #[test_case(None, Some("debug"), "debug" ; "shared_variable_with_a_level_is_taken_as_is")]
+    #[test_case(None, Some(FOREIGN), "info" ; "foreign_directive_cannot_silence_us")]
+    #[test_case(None, Some(FOREIGN_EMPTY_PIECES), "info" ; "empty_pieces_are_not_a_level")]
+    #[test_case(None, Some(BROKEN), "info" ; "unparsable_value_falls_back")]
+    fn log_filter_keeps_our_own_logs_alive(
+        ours: Option<&str>,
+        shared: Option<&str>,
+        expected: &str,
+    ) {
+        let rendered = build_log_filter(ours, shared).to_string();
+        assert!(
+            rendered.split(',').any(|d| d == expected),
+            "{ours:?} + {shared:?} rendered as {rendered:?}, wanted {expected:?}"
+        );
+    }
+
+    #[test]
+    fn foreign_directive_is_kept_alongside_our_level() {
+        let rendered = build_log_filter(None, Some(FOREIGN)).to_string();
+        assert!(rendered.contains(FOREIGN), "{FOREIGN} was dropped");
+        assert!(rendered.contains(&DEFAULT_LOG_LEVEL.to_string()));
+    }
 }

lun-4/makima

maki-lua/src/docs_render.rs
@@ -132,9 +132,11 @@ settings in a local table, or export a `setup(opts)` function `init.lua` calls.
 runs, an edited plugin is still the old one.
 
 To debug, add `maki.log.info|warn|error(...)` calls. They write to `maki.log`
-in the dir `maki.env.logs_dir()` returns (Linux: `~/.local/logs/makima/`). When
-a backtrace comes out useless, start makima with `--no-jit`: plugins then run on
-the interpreter, with full debug info.
+in the dir `maki.env.logs_dir()` returns (Linux: `~/.local/logs/makima/`). The
+log keeps `info` and above. Set `MAKI_LOG=debug` to also keep `maki.log.debug`,
+or `MAKI_LOG=maki_lua=trace` to narrow it to one target. When a backtrace comes
+out useless, start makima with `--no-jit`: plugins then run on the interpreter,
+with full debug info.
 
 {AGENT_NOTES}## Conventions
 

src/setup.rs
@@ -1,4 +1,5 @@
-use std::sync::Mutex;
+use std::io::{self, Write};
+use std::sync::{Mutex, MutexGuard, PoisonError};
 
 use color_eyre::Result;
 use color_eyre::eyre::{Context, eyre};
@@ -9,6 +10,12 @@ use maki_storage::StateDir;
 use maki_storage::log::RotatingFileWriter;
 use maki_storage::model::read_model;
 use tracing_subscriber::EnvFilter;
+use tracing_subscriber::filter::LevelFilter;
+use tracing_subscriber::fmt::MakeWriter;
+
+const LOG_ENV: &str = "MAKI_LOG";
+const LOG_ENV_SHARED: &str = "RUST_LOG";
+const DEFAULT_LOG_LEVEL: LevelFilter = LevelFilter::INFO;
 
 const PROVIDER_PRIORITY: &[&str] = &[
     "anthropic",
@@ -134,17 +141,129 @@ pub fn install_panic_log_hook() {
     }));
 }
 
+/// The writer every event goes through.
+///
+/// A bare `Mutex` would do, except `tracing_subscriber`'s blanket impl unwraps
+/// the poison, so a single panic raised while a line was being written turned
+/// every later log call into a panic of its own. Taking the guard back out of
+/// the poison keeps a wounded process writing its own post mortem.
+struct SharedWriter(Mutex<RotatingFileWriter>);
+
+struct SharedGuard<'a>(MutexGuard<'a, RotatingFileWriter>);
+
+impl Write for SharedGuard<'_> {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        self.0.write(buf)
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        self.0.flush()
+    }
+}
+
+impl<'a> MakeWriter<'a> for SharedWriter {
+    type Writer = SharedGuard<'a>;
+
+    fn make_writer(&'a self) -> Self::Writer {
+        SharedGuard(self.0.lock().unwrap_or_else(PoisonError::into_inner))
+    }
+}
+
+/// `MAKI_LOG` is ours and is taken literally, `MAKI_LOG=off` included.
+///
+/// `RUST_LOG` belongs to every Rust program on the machine, and `EnvFilter`
+/// drops any target no directive names, so a `RUST_LOG=other_crate=debug` left
+/// in a shell profile for an unrelated project silenced maki entirely. We still
+/// read it, and a value that names a level decides ours, but a value that only
+/// lists other people's targets gets our level added underneath, so a foreign
+/// variable can add to our logs and never take them away.
+fn log_filter() -> EnvFilter {
+    let read = |name: &str| std::env::var(name).ok().filter(|v| !v.trim().is_empty());
+    build_log_filter(read(LOG_ENV).as_deref(), read(LOG_ENV_SHARED).as_deref())
+}
+
+/// Pure core of `log_filter`, so tests never have to set a process wide
+/// variable and race every other test in the binary.
+fn build_log_filter(ours: Option<&str>, shared: Option<&str>) -> EnvFilter {
+    for (name, value, is_ours) in [(LOG_ENV, ours, true), (LOG_ENV_SHARED, shared, false)] {
+        let Some(value) = value else {
+            continue;
+        };
+        match EnvFilter::builder().parse(value) {
+            Ok(filter) if is_ours || sets_a_level(value) => return filter,
+            Ok(filter) => return filter.add_directive(DEFAULT_LOG_LEVEL.into()),
+            Err(error) => eprintln!("makima: ignoring {name}={value:?}: {error}"),
+        }
+    }
+    EnvFilter::default().add_directive(DEFAULT_LOG_LEVEL.into())
+}
+
+/// Whether a filter string carries a bare level such as `debug`, as opposed to
+/// only per target directives like `foo=debug`.
+///
+/// Empty pieces are skipped here, as `EnvFilter` skips them too. `LevelFilter`
+/// reads an empty string as `error`, so the trailing comma in
+/// `RUST_LOG=other_crate=debug,` would look like a level and cost us the log.
+fn sets_a_level(value: &str) -> bool {
+    value
+        .split(',')
+        .map(str::trim)
+        .any(|part| !part.is_empty() && part.parse::<LevelFilter>().is_ok())
+}
+
+/// Logging is the first thing to set up and the last thing allowed to stop the
+/// program, but a failure here used to be swallowed whole, so an unwritable log
+/// dir looked exactly like an idle one: no logs, no reason, no clue.
 pub fn init_logging(storage_config: &maki_config::StorageConfig) {
-    let Ok(writer) =
-        RotatingFileWriter::new(storage_config.max_log_bytes, storage_config.max_log_files)
-    else {
-        return;
-    };
-    let writer = Mutex::new(writer);
-    let filter = EnvFilter::try_from_env("RUST_LOG").unwrap_or_else(|_| EnvFilter::new("info"));
+    let writer =
+        match RotatingFileWriter::new(storage_config.max_log_bytes, storage_config.max_log_files) {
+            Ok(writer) => writer,
+            Err(error) => {
+                eprintln!("makima: logging disabled, cannot open the log file: {error}");
+                return;
+            }
+        };
     tracing_subscriber::fmt()
         .json()
-        .with_env_filter(filter)
-        .with_writer(writer)
+        .with_env_filter(log_filter())
+        .with_writer(SharedWriter(Mutex::new(writer)))
         .init();
+}
+
+#[cfg(test)]
+mod tests {
+    use test_case::test_case;
+
+    use super::{DEFAULT_LOG_LEVEL, build_log_filter};
+
+    const FOREIGN: &str = "other_crate=debug";
+    const FOREIGN_EMPTY_PIECES: &str = ",other_crate=debug,,";
+    const BROKEN: &str = "not a directive=!!";
+
+    #[test_case(None, None, "info" ; "nothing_set_logs_at_info")]
+    #[test_case(Some("warn"), None, "warn" ; "our_variable_wins")]
+    #[test_case(Some("off"), None, "off" ; "our_variable_can_silence_us")]
+    #[test_case(Some("warn"), Some("trace"), "warn" ; "our_variable_beats_the_shared_one")]
+    #[test_case(None, Some("debug"), "debug" ; "shared_variable_with_a_level_is_taken_as_is")]
+    #[test_case(None, Some(FOREIGN), "info" ; "foreign_directive_cannot_silence_us")]
+    #[test_case(None, Some(FOREIGN_EMPTY_PIECES), "info" ; "empty_pieces_are_not_a_level")]
+    #[test_case(None, Some(BROKEN), "info" ; "unparsable_value_falls_back")]
+    fn log_filter_keeps_our_own_logs_alive(
+        ours: Option<&str>,
+        shared: Option<&str>,
+        expected: &str,
+    ) {
+        let rendered = build_log_filter(ours, shared).to_string();
+        assert!(
+            rendered.split(',').any(|d| d == expected),
+            "{ours:?} + {shared:?} rendered as {rendered:?}, wanted {expected:?}"
+        );
+    }
+
+    #[test]
+    fn foreign_directive_is_kept_alongside_our_level() {
+        let rendered = build_log_filter(None, Some(FOREIGN)).to_string();
+        assert!(rendered.contains(FOREIGN), "{FOREIGN} was dropped");
+        assert!(rendered.contains(&DEFAULT_LOG_LEVEL.to_string()));
+    }
 }